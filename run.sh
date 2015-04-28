90313ed5e984d4841ba5fabe5d63ab6ad31a3f9a
Sebastian Kippe <sebastian@kip.pe>
2015-04-28 23:48:14 +0200

    Only join #kosmos-dev when testing HAL

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 HUBOT_IRC_SERVER=irc.freenode.net \
-  HUBOT_IRC_ROOMS="#67p,#kosmos,#kosmos-dev" \
+  HUBOT_IRC_ROOMS="##kosmos-dev" \
   HUBOT_IRC_NICK="hal7000" \
   HUBOT_IRC_UNFLOOD="true" \
   bin/hubot -a irc --name hal7000
