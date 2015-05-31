ac8e5d4ac1d2cc6eddcf49ccbdd48b82152e2e3d
Sebastian Kippe <sebastian@kip.pe>
2015-06-01 00:41:56 +0200

    Add auth and plusplus

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ HUBOT_IRC_SERVER=irc.freenode.net \
   HUBOT_IRC_ROOMS="#kosmos-dev" \
   HUBOT_IRC_NICK="hal7000" \
   HUBOT_IRC_UNFLOOD="true" \
+  HUBOT_AUTH_ADMIN="bkero,derbumi,galfert,gregkare,jaaan,slvrbckt,raucao" \
   bin/hubot -a irc --name hal7000
