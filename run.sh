c405c3b94164f507952a56411a120840aff4aaeb
Sebastian Kippe <sebastian@kip.pe>
2015-07-07 17:19:48 +0200

    Use new rs-logger-script instead of previous hack

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 # cp ~/src/remotestorage/modules/src/chat-messages.js ~/src/kosmos/hubot-logger/lib/chat-messages.es6
-# rm -rf node_modules/hubot-logger
+# rm -rf node_modules/hubot-remotestorage-logger
 # npm install
 
 HUBOT_IRC_SERVER="irc.freenode.net" \
 HUBOT_IRC_ROOMS="#kosmos-dev,#kosmos-random" \
 HUBOT_IRC_NICK="hal7000" \
-HUBOT_IRC_UNFLOOD="true" \
+HUBOT_IRC_UNFLOOD="300" \
 HUBOT_RSS_PRINTSUMMARY="false" \
 HUBOT_RSS_IRCCOLORS="true" \
 HUBOT_RSS_HEADER="Update:" \
 HUBOT_AUTH_ADMIN="bkero,derbumi,galfert,gregkare,jaaan,slvrbckt,raucao" \
 LOG_HTTP_PORT=7000 \
 LOG_STEALTH="true" \
-LOG_RS_USER="kosmos@5apps.com" \
-LOG_RS_TOKEN="change-me" \
-LOG_RS_SERVER_NAME="freenode" \
-LOG_RS_PUBLIC="true" \
+RS_LOGGER_USER="kosmos@5apps.com" \
+RS_LOGGER_TOKEN="change-me" \
+RS_LOGGER_SERVER_NAME="freenode" \
+RS_LOGGER_PUBLIC="true" \
 bin/hubot -a irc --name hal7000
